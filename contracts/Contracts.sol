31fd55f0349f7ff4b53db5e4abc9103e18277712
Jeremy Kale Padot <jeremy@padot.us>
2023-05-10 21:30:24 -0600

    revert

--- contracts/Contracts.sol
+++ contracts/Contracts.sol
@@ -1,30 +1,30 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC721Base.sol";
-import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
+// import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+// import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
-contract MyAwesomeNFTs is ERC721Base, PermissionsEnumerable {
+// contract MyAwesomeNFTs is ERC721Base, PermissionsEnumerable {
 
-// mapping from token ID to the power level of the NFT
+// // mapping from token ID to the power level of the NFT
 
-      constructor(
-        string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    )
-        ERC721Base(
-            _name,
-            _symbol, 
-            _royaltyRecipient,
-            _royaltyBps
-        )
-    {
+//       constructor(
+//         string memory _name,
+//         string memory _symbol,
+//         address _royaltyRecipient,
+//         uint128 _royaltyBps
+//     )
+//         ERC721Base(
+//             _name,
+//             _symbol, 
+//             _royaltyRecipient,
+//             _royaltyBps
+//         )
+//     {
 
-        set up the DEFAULT ADMIN ROLE 
-        provide that role to the wallet deployer
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
+//         set up the DEFAULT ADMIN ROLE 
+//         provide that role to the wallet deployer
+//         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+//     }
 
-}
+// }
